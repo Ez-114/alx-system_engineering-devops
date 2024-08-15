9a7ff312627134695f7f1c9439524f9a00d4555a
Ez-114 <morganezz226@gmail.com>
2024-08-16 02:33:10 +0300

    final fix

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -15,44 +15,21 @@ Args:
                     to-do list information is to be retrieved.
 """
 
-import json
+import requests
 import sys
-from urllib.request import urlopen
 
 
 if __name__ == "__main__":
 
-    # Get employee id from command-line
-    emp_id = sys.argv[1] if len(sys.argv) >= 2 else exit()
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-    # Format URLs
-    todo_url = f"https://jsonplaceholder.typicode.com/todos?userId={emp_id}"
-    emp_url = f"https://jsonplaceholder.typicode.com/users/{emp_id}"
-
-    # Request employee name
-    with urlopen(emp_url) as response:
-        emp_name = json.loads(response.read()).get('name')
-
-
-    # Request employee todos
-    with urlopen(todo_url) as response:
-        todos = json.loads(response.read())
-
-
-    # Completed todo titles
-    todo_titles = []
-    total_completed = 0
-    total_todos = 0
-    for todo in todos:
-        total_todos += 1
-
-        if todo['completed'] is True:
-            total_completed += 1
-            todo_titles.append(todo['title'])
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
 
     # Print formatted text
-    print("Employee {} is done with \
-    tasks({}/{}):".format(emp_name, total_completed, total_todos))
+    print("Employee {} is done with tasks({}/{}):".format(
+                        user.get('name'), len(completed), len(todos)))
 
-    for title in todo_titles:
-        print("\t {}".format(title))
+    for c in completed:
+        print("\t {}".format(c))
