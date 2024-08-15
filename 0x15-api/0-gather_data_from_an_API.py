83bd6c6187e58e5b15b12b7bd8da895ccb9d4d0c
Ez-114 <morganezz226@gmail.com>
2024-08-16 02:25:39 +0300

    solved docstring problems

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
-"""0-gather_data_from_as_API
 
-This script makes a HTTP GET request to jsonplaceholder
-to recive a given employee's todo-list info
+"""
+0-gather_data_from_as_API
+
+This script retrieves a given employee's to-do list information from
+the JSONPlaceholder API and prints the number of completed tasks along
+with their titles.
+
+Usage:
+    python3 0-gather_data_from_as_API <employee_id>
+
+Args:
+    employee_id (int): The ID of the employee whose
+                    to-do list information is to be retrieved.
 """
 
 import json
@@ -10,37 +20,39 @@ import sys
 from urllib.request import urlopen
 
 
-# Get employee id from command-line
-emp_id = sys.argv[1] if len(sys.argv) >= 2 else exit()
+if __name__ == "__main__":
 
-# Format URLs
-todo_url = f"https://jsonplaceholder.typicode.com/todos?userId={emp_id}"
-emp_url = f"https://jsonplaceholder.typicode.com/users/{emp_id}"
+    # Get employee id from command-line
+    emp_id = sys.argv[1] if len(sys.argv) >= 2 else exit()
 
-# Request employee name
-with urlopen(emp_url) as response:
-    emp_name = json.loads(response.read())['name']
+    # Format URLs
+    todo_url = f"https://jsonplaceholder.typicode.com/todos?userId={emp_id}"
+    emp_url = f"https://jsonplaceholder.typicode.com/users/{emp_id}"
 
-
-# Request employee todos
-with urlopen(todo_url) as response:
-    todos = json.loads(response.read())
+    # Request employee name
+    with urlopen(emp_url) as response:
+        emp_name = json.loads(response.read()).get('name')
 
 
-# Completed todo titles
-todo_titles = []
-total_completed = 0
-total_todos = 0
-for todo in todos:
-    total_todos += 1
+    # Request employee todos
+    with urlopen(todo_url) as response:
+        todos = json.loads(response.read())
 
-    if todo['completed'] is True:
-        total_completed += 1
-        todo_titles.append(todo['title'])
 
-# Print formatted text
-print("Employee {} is done with \
-tasks({}/{}):".format(emp_name, total_completed, total_todos))
+    # Completed todo titles
+    todo_titles = []
+    total_completed = 0
+    total_todos = 0
+    for todo in todos:
+        total_todos += 1
 
-for title in todo_titles:
-    print("\t {}".format(title))
+        if todo['completed'] is True:
+            total_completed += 1
+            todo_titles.append(todo['title'])
+
+    # Print formatted text
+    print("Employee {} is done with \
+    tasks({}/{}):".format(emp_name, total_completed, total_todos))
+
+    for title in todo_titles:
+        print("\t {}".format(title))
