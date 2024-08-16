7caf6f89b413ce67f9c5935dbd7eacf52b13d29a
Ez-114 <morganezz226@gmail.com>
2024-08-16 06:01:12 +0300

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -20,7 +20,7 @@ def number_of_subscribers(subreddit):
     """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
 
-    headers={"User-Agent": "Custom"}
+    headers = {"User-Agent": "Custom"}
 
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
